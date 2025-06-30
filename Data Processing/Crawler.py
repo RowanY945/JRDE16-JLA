ea67b25feca6145ad3fcb0c5e8f909c757a98fbc
georedddd <georged6513@yahoo.com.au>
2025-06-30 21:01:14 +0800

    Add new files to george branch

---
+++ Data Processing/Crawler.py
@@ -0,0 +1,38 @@
+from open_linkedin_api import Linkedin
+def get_j(self, job_id: str):
+        """Fetch data about a given job.
+        :param job_id: LinkedIn job ID
+        :type job_id: str
+
+        :return: Job data
+        :rtype: dict
+        """
+        params = {
+            "decorationId": "com.linkedin.voyager.deco.jobs.web.shared.WebFullJobPosting-63",
+        }
+
+        res = self._fetch(f"/jobs/jobPostings/{job_id}", params=params)
+
+        data = res.json()
+
+        if data and "status" in data and data["status"] != 200:
+            self.logger.info("request failed: {}".format(data["message"]))
+            return {}
+
+        return data 
+Linkedin.get_job=get_j
+lk=Linkedin(username='datajr16001@gmail.com',password='Datajr@16')
+def get_jobid(js):
+    
+      return js['trackingUrn'].split(':')[-1]
+      
+def get_jobdescription(js):
+      return js['description']['text']
+#demo
+ak=lk.search_jobs(keywords="data engineer")
+jobid=get_jobid(ak[0])
+print(jobid)
+jobfull=lk.get_job(job_id=jobid)
+print(jobfull)
+jd=get_jobdescription(jobfull)
+print(jd)
