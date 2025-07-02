74d80574a1ae7903ecee46252f6a35191c782ae5
georedddd <georged6513@yahoo.com.au>
2025-07-02 17:48:20 +0800

    change evade

--- Data Processing/Crawler.py
+++ Data Processing/Crawler.py
@@ -20,7 +20,15 @@ def get_j(self, job_id: str):
             return {}
 
         return data 
+def new_evade():
+    """
+    A catch-all method to try and evade suspension from Linkedin.
+    Currenly, just delays the request by a random (bounded) time
+    """
+    sleep(random.randint(2, 4))  # sleep a random duration to try and evade suspention
+
 Linkedin.get_job=get_j
+Linkedin.default_evade=new_evade
 lk=Linkedin(username='datajr16001@gmail.com',password='Datajr@16')
 def get_jobid(js):
     
