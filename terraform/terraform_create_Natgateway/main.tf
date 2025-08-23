bafb7b6d86379ef3329aa6f7aa83529df42d7873
georedddd <georged6513@yahoo.com.au>
2025-08-23 15:58:23 +1000

    all

--- terraform/terraform_create_Natgateway/main.tf
+++ terraform/terraform_create_Natgateway/main.tf
@@ -33,12 +33,3 @@ output "lambda_name" {
   value = aws_lambda_function.demo.function_name
 }
 
-resource "aws_iam_openid_connect_provider" "default" {
-  url = "https://accounts.google.com"
-
-  client_id_list = [
-    "266362248691-342342xasdasdasda-apps.googleusercontent.com",
-  ]
-
-  thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"]
-}
