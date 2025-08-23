ea7dd254315fe3a223312937c7d1216bee99e9b0
georedddd <georged6513@yahoo.com.au>
2025-08-23 15:37:22 +1000

    all

--- terraform/terraform_delete_Natgateway/variables.tf
+++ terraform/terraform_delete_Natgateway/variables.tf
@@ -1,4 +1,3 @@
-
 variable "region" {
   description = "AWS region"
   type        = string
@@ -13,7 +12,7 @@ variable "function_name_create" {
 variable "function_name_delete" {
   description = "Databricks-Natgateway-elasticIP-delete"
   type        = string
-  default="Databricks-Natgateway-elasticIP-Delete"
+  default= "Databricks-Natgateway-elasticIP-delete"
 }
 
 
@@ -60,66 +59,3 @@ variable "environment_variables" {
   type        = map(string)
   default     = {}
 }
-
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-southeast-2"
-}
-
-variable "function_name_create" {
-  description = "Databricks-Natgateway-elasticIP-Create"
-  type        = string
-  default="Databricks-Natgateway-elasticIP-Create"
-}
-variable "function_name_delete" {
-  description = "Databricks-Natgateway-elasticIP-delete"
-  type        = string
-  default="Databricks-Natgateway-elasticIP-Delete"
-}
-
-
-
-
-variable "package_path"{
-    description="the location of lambda code"
-    type=string
-    default="./package.zip"
-}
-
-variable "handler" {
-  description = "whch is to commnet"
-  type        = string
-  default="main.handler"
-}
-
-variable "runtime" {
-  description = "Lambda env used"
-  type        = string
-  default="python3.11"
-}
-
-variable "memory_size" {
-  description = "memory size for lambda"
-  type        = number
-  default     = 512
-}
-
-variable "timeout" {
-  description = "Lambda timeout。"
-  type        = number
-  default     = 900
-}
-
-variable "architectures" {
-  description = "arm64 save more resouce"
-  type        = list(string)
-  default     = ["arm64"]
-}
-
-variable "environment_variables" {
-  description = "pass for env"
-  type        = map(string)
-  default     = {}
-}
->>>>>>> 27dc2eb88286d4d1e425f957b4627d62bfb0cce6
