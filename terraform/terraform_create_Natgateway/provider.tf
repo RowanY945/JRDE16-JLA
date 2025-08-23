0d9dc5dc5584b7d53969f6ebeb7b45ea0f26d210
georedddd <georged6513@yahoo.com.au>
2025-08-23 15:57:35 +1000

    all

--- terraform/terraform_create_Natgateway/provider.tf
+++ terraform/terraform_create_Natgateway/provider.tf
@@ -11,14 +11,12 @@ terraform {
 }
 
 # AWS Provider configuration
-provider "aws" {
-  region = var.region
-  
-  default_tags {
-    tags = {
-      Project     = "JobProcessing"
-      Environment = "production"
-      ManagedBy   = "Terraform"
-    }
-  }
+resource "aws_iam_openid_connect_provider" "default" {
+  url = "https://accounts.google.com"
+
+  client_id_list = [
+    "266362248691-342342xasdasdasda-apps.googleusercontent.com",
+  ]
+
+  thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"]
 }
