a8b6165b0cc8961c3668daa1ee75e21195c04e25
georedddd <georged6513@yahoo.com.au>
2025-08-23 23:00:37 +1000

    ver1

---
+++ jra_dbt/models/test/indeed_cleaned.sql
@@ -0,0 +1,46 @@
+{{ 
+    config(
+        materialized='incremental',
+        file_format='delta',
+        location_root='s3://jla-data-silver/',
+        unique_key='job_posting_id',
+        incremental_strategy='merge',
+        partition_by=['year', 'month', 'day']
+    ) 
+}}
+
+SELECT 
+    url, 
+    job_posting_id, 
+    job_title, 
+    company_name, 
+    job_location, 
+    job_summary,
+    job_seniority_level, 
+    job_function, 
+    job_employment_type, 
+
+    job_industries,
+    
+    MIN_AMOUNT as min_salary, 
+    MAX_AMOUNT as max_salary, 
+    to_date(job_posted_date) AS job_posted_date, 
+    timestamp AS scraped_dts, 
+    ingest_dts,
+    _rescued_data,
+    0 AS is_enriched, 
+    0 AS job_source, 
+    1 AS is_active,
+    -- Partition columns based on scraped_time
+    year(current_date()) as year,
+    month(current_date()) as month,
+    day(current_date()) as day
+
+FROM {{ source('bronze_layer', 'indeed_test') }}
+
+{% if is_incremental() %}
+    WHERE timestamp > (
+        SELECT COALESCE(MAX(ingest_dts), '1900-01-01'::timestamp)
+        FROM {{ this }}
+    )
+{% endif %}
