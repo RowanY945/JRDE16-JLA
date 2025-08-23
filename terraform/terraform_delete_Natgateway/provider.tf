40a9dcf7ddbb60cb38eafbe46adbe04384367e8d
georedddd <georged6513@yahoo.com.au>
2025-08-23 17:06:43 +1000

    all

--- terraform/terraform_delete_Natgateway/provider.tf
+++ terraform/terraform_delete_Natgateway/provider.tf
@@ -11,12 +11,6 @@ terraform {
 }
 
 # AWS Provider configuration
-resource "aws_iam_openid_connect_provider" "default" {
-  url = "https://accounts.google.com"
-
-  client_id_list = [
-    "266362248691-342342xasdasdasda-apps.googleusercontent.com",
-  ]
-
-  thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"]
+provider "aws" {
+  region = "ap-southeast-2"  
 }
